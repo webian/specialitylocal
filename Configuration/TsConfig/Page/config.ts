266e8a6ed707a128bd1c64855c9e0289b0b8781c
Ian Excedo <ian@webian.it>
2014-02-05 10:04:36 +0100

    [FEATURE] Add Page & User TS

---
+++ Configuration/TsConfig/Page/config.ts
@@ -0,0 +1,1 @@
+<INCLUDE_TYPOSCRIPT: source="FILE:EXT:speciality/Configuration/TsConfig/Page/rte.ts">
--- ext_tables.php
+++ ext_tables.php
@@ -1,8 +1,18 @@
 <?php
-if (!defined('TYPO3_MODE')) {
+if (!defined ('TYPO3_MODE')) {
 	die ('Access denied.');
 }
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript', 'Customize Speciality Local');
+
+# Add page TSConfig
+$pageTsConfig = \TYPO3\CMS\Core\Utility\GeneralUtility::getUrl(
+	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY) . 'Configuration/TsConfig/Page/config.ts');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig($pageTsConfig);
+
+# Add user TSConfig
+$userTsConfig = \TYPO3\CMS\Core\Utility\GeneralUtility::getUrl(
+	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY) . 'Configuration/TsConfig/User/config.ts');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addUserTSConfig($userTsConfig);
 
 ?>
